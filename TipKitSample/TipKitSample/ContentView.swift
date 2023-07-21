cdf4e09b071f22ee5f731967fa629217e65165c9
cyeond <cyeond@gmail.com>
2023-07-22 02:00:50 +0900

    샘플 기반 구성

---
+++ TipKitSample/TipKitSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipKitSample
+//
+//  Created by YD on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipKitSample/TipKitSample/TipKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitSampleApp.swift
+//  TipKitSample
+//
+//  Created by YD on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TipKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
